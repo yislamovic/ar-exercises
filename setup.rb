49d55ba026fa732b5dfd20b45b5b4b89379c10e6
Yahya Islamovic <yahya.islamovic.work@live.com>
2021-09-13 22:23:14 -0400

    init

--- setup.rb
+++ setup.rb
@@ -10,8 +10,8 @@ puts 'Establishing connection to database ...'
 ActiveRecord::Base.establish_connection(
   adapter: 'postgresql',
   database: 'ar_exercises',
-  username: 'development',
-  password: 'development',
+  username: 'postgres',
+  password: 'airman',
   host: 'localhost',
   port: 5432,
   pool: 5,
